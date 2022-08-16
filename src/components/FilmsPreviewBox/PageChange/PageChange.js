4d98d7b2599f7554f9a225c266b5c5a8be1d9a5d
Dima <dimaziboc@gmail.com>
2022-08-16 20:03:59 +0300

    переключение страницы на предыдущую или следующую при клике на кнопку

--- src/components/FilmsPreviewBox/PageChange/PageChange.js
+++ src/components/FilmsPreviewBox/PageChange/PageChange.js
@@ -6,6 +6,11 @@ function PageChange(props) {
     const pageNum = props.pageNum;
     const setPagefunc = props.setPageFunc;
 
+    function changePage(newValue) {
+        props.toScrollElement.current.scrollIntoView({ block: "center" });
+        setPagefunc(newValue);
+    }
+
     function createPageChangeButtons() {
         let lastPages = [],
             nextPages = [];
@@ -20,37 +25,40 @@ function PageChange(props) {
             <div className="page-num-buttons">
                 {lastPages.map((item) => {
                     return (
-                        <PageNumberButton buttonNum={item} key={item} />
+                        <PageNumberButton changePage={changePage} buttonNum={item} key={item} />
                     )
                 })}
                 <button className="active-page">{pageNum}</button>
                 {nextPages.map((item) => {
                     return (
-                        <PageNumberButton buttonNum={item} key={item} />
+                        <PageNumberButton changePage={changePage} buttonNum={item} key={item} />
                     )
                 })}
             </div>
         )
     }
 
-    function PageNumberButton(propsOnclickFunc) {
-
-        function changePage() {
-            props.toScrollElement.current.scrollIntoView({ block: "center" });
-            setPagefunc(propsOnclickFunc.buttonNum)
-        }
-
-        return (
-            <button className="PageNumberButton" onClick={changePage}>{propsOnclickFunc.buttonNum}</button>
-        )
-    }
-
     return (
         <div className={"page-change-wrapper " + (pagesCount > 1 ? "" : "noVisible")}>
-            <button className="last-page change-page-arrowButton"><div className="arrowBox"><span></span></div></button>
+
+            <button className="last-page change-page-arrowButton"
+                onClick={() => { changePage(pageNum - 1) }}
+            ><div className="arrowBox"><span></span></div></button>
+
             {createPageChangeButtons()}
-            <button className="next-page change-page-arrowButton"><div className="arrowBox"><span></span></div></button>
+
+            <button className="next-page change-page-arrowButton"
+                onClick={() => { changePage(pageNum + 1) }}
+            ><div className="arrowBox"><span></span></div></button>
+
         </div>
+    )
+}
+
+function PageNumberButton(propsOnclickFunc) {
+    const changePage = propsOnclickFunc.changePage;
+    return (
+        <button className="PageNumberButton" onClick={() => { changePage(propsOnclickFunc.buttonNum) }}>{propsOnclickFunc.buttonNum}</button>
     )
 }
 
